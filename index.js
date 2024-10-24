e80a592bd0dcfcf8c7d57cee23f5b9ced3928fc1
Mahad Ahmed <Mahadahmed99@yahoo.com>
2024-10-24 19:52:33 +0000

    add /api/fileanalyse endpoint

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var cors = require('cors');
+const multer = require('multer');
 require('dotenv').config()
+
+const upload = multer({ dest: './public/data/uploads/' })
 
 var app = express();
 
@@ -12,6 +15,15 @@ app.get('/', function (req, res) {
 });
 
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  // console.log(req.body)
+  // console.log(req.file, req.body)
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  })
+});
 
 
 const port = process.env.PORT || 3000;
